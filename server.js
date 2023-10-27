385f508601028c70826a288f553d07a09cd60e61
sugiyan0519 <b1019261@fun.ac.jp>
2023-10-27 20:27:38 +0900

    feat: 図の削除、pdf一覧、ファイルのアップロード

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
- 
+
 const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
@@ -24,7 +24,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render("shotting.ejs");
 });
-app.post('/index', multer({dest: "./upload"}).single("clip"), (req, res) => {
+app.post('/index', multer({ dest: "./upload" }).single("clip"), (req, res) => {
   // ここで blob が bufferになって保存される
   clipBlobPath = req.file.path;
   res.send();
@@ -33,11 +33,50 @@ app.post('/index', multer({dest: "./upload"}).single("clip"), (req, res) => {
 app.get('/index', (req, res) => {
   res.render("index.ejs");
 });
-app.get('/clip', (req, res)=>{
+app.get('/clip', (req, res) => {
   // console.log(fs.readFileSync(clipBlobPath));
   // res.send(fs.readFileSync(clipBlobPath));
   res.sendFile(path.join(__dirname, clipBlobPath));
 });
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+var i = 0;
+
+app.get("/pdf-list-names", (req, res) => {
+  fs.readdir("./uploads", (err, files) => {
+    res.send(files.join());
+  });
+});
+
+app.get("/pdf-list", (req, res) => {
+  fs.readdir("./uploads", (err, files) => {
+    res.sendFile(path.join(__dirname, "./uploads/"+files[i++]));
+    if(i == files.length) i = 0;
+    //files.forEach(file=>res.sendFile(path.join(__dirname, "./uploads/"+file)));
+    //res.send(files.join());
+  });
+});
+
+app.get("/reset", (req, res) => {
+  i = 0;
+});
+
+const pdfStrage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+app.post("/pdf-upload", multer({storage: pdfStrage}).single("pdf"), function(req, res) {
+  if(req.file) {
+    console.log(req.file.path);
+    res.sendFile(path.join(__dirname, req.file.path));
+  } else {
+    console.log("faild");
+  }
 });
